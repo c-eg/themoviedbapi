cf0e766d58c2f781d0a1b119f06afc17bb968fe8
Conor Egan <68134729+c-eg@users.noreply.github.com>
2023-05-19 21:11:12 +0100

    add module info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module info.movito.themoviedbapi {
+    requires org.slf4j;
+    requires org.apache.commons.lang3;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+}
