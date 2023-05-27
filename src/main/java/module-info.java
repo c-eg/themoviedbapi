f7e7998262d6c0910c9f78213d6347b8334a0934
Conor Egan <68134729+c-eg@users.noreply.github.com>
2023-05-27 20:06:48 +0100

    fix module info missing requirement + not exporting

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,19 @@
 module info.movito.themoviedbapi {
     requires org.slf4j;
+    requires org.apache.commons.codec;
     requires org.apache.commons.lang3;
     requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
+
+    exports info.movito.themoviedbapi;
+    exports info.movito.themoviedbapi.model;
+    exports info.movito.themoviedbapi.model.changes;
+    exports info.movito.themoviedbapi.model.config;
+    exports info.movito.themoviedbapi.model.core;
+    exports info.movito.themoviedbapi.model.keywords;
+    exports info.movito.themoviedbapi.model.people;
+    exports info.movito.themoviedbapi.model.providers;
+    exports info.movito.themoviedbapi.model.tv;
+    exports info.movito.themoviedbapi.tools;
 }
