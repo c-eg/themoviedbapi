f586d04a1a398ee1aaef54a3d724283aab276164
Conor Egan <68134729+c-eg@users.noreply.github.com>
2023-10-29 18:01:19 +0000

    fix module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,21 @@ module info.movito.themoviedbapi {
     requires org.slf4j;
     requires org.apache.commons.codec;
     requires org.apache.commons.lang3;
+    requires com.google.common;
     requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
+
+    opens info.movito.themoviedbapi to com.fasterxml.jackson.databind;
+    opens info.movito.themoviedbapi.model to com.fasterxml.jackson.databind;
+    opens info.movito.themoviedbapi.model.changes to com.fasterxml.jackson.databind;
+    opens info.movito.themoviedbapi.model.config to com.fasterxml.jackson.databind;
+    opens info.movito.themoviedbapi.model.core to com.fasterxml.jackson.databind;
+    opens info.movito.themoviedbapi.model.keywords to com.fasterxml.jackson.databind;
+    opens info.movito.themoviedbapi.model.people to com.fasterxml.jackson.databind;
+    opens info.movito.themoviedbapi.model.providers to com.fasterxml.jackson.databind;
+    opens info.movito.themoviedbapi.model.tv to com.fasterxml.jackson.databind;
+    opens info.movito.themoviedbapi.tools to com.fasterxml.jackson.databind;
 
     exports info.movito.themoviedbapi;
     exports info.movito.themoviedbapi.model;
