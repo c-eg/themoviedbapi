965543ee8d2e9038569f1c2dbdcf316e5fe56d9f
Conor Egan <68134729+c-eg@users.noreply.github.com>
2023-12-22 21:10:22 +0000

    checkstyle

--- src/main/java/info/movito/themoviedbapi/TmdbGenre.java
+++ src/main/java/info/movito/themoviedbapi/TmdbGenre.java
@@ -71,7 +71,8 @@ public class TmdbGenre extends AbstractTmdbApi {
      *
      * This prevents movies from 1 10/10 rating from being listed first and for the first 5 pages.
      */
-    public MovieDbResultsPage getGenreMovies(Integer genreId, String language, Integer page, Boolean includeAllMovies) throws TmdbException {
+    public MovieDbResultsPage getGenreMovies(Integer genreId, String language, Integer page, Boolean includeAllMovies)
+        throws TmdbException {
         ApiEndpoint apiEndpoint = new ApiEndpoint(TMDB_METHOD_GENRE, genreId, "movies");
 
         apiEndpoint.addLanguage(language);
