34c26dc991a75816d30859749bb6a3f64a95c3e0
Conor Egan <68134729+c-eg@users.noreply.github.com>
2024-01-01 16:50:28 +0000

    remove lombok annotation from record

--- src/main/java/info/movito/themoviedbapi/model/core/SessionToken.java
+++ src/main/java/info/movito/themoviedbapi/model/core/SessionToken.java
@@ -1,16 +1,11 @@
 package info.movito.themoviedbapi.model.core;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
 /**
  * A simple wrapper around the session token used for user authentication. Introduced to have a more strongly typed
  * api.
  *
  * @param sessionToken the session token.
  */
-@Data
-@EqualsAndHashCode(callSuper = false)
 public record SessionToken(String sessionToken) {
     public SessionToken {
         assert sessionToken != null;
