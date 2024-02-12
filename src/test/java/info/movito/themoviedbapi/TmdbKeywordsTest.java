57b7756193e26b440cd39a62b62e8c8d08e701f8
Conor Egan <68134729+c-eg@users.noreply.github.com>
2024-02-12 01:58:33 +0000

    test keywords api

---
+++ src/test/java/info/movito/themoviedbapi/TmdbKeywordsTest.java
@@ -0,0 +1,36 @@
+package info.movito.themoviedbapi;
+
+import info.movito.themoviedbapi.model.keywords.Keyword;
+import info.movito.themoviedbapi.tools.RequestType;
+import info.movito.themoviedbapi.tools.TmdbException;
+import info.movito.themoviedbapi.util.TestUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static info.movito.themoviedbapi.TmdbKeywords.TMDB_METHOD_KEYWORD;
+import static info.movito.themoviedbapi.tools.ApiUrl.TMDB_API_BASE_URL;
+import static info.movito.themoviedbapi.util.TestUtils.testForNullFieldsAndNewItems;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests for {@link TmdbKeywords}.
+ */
+public class TmdbKeywordsTest extends AbstractTmdbApiTest {
+    /**
+     * Tests {@link TmdbKeywords#getDetails(int)} with an expected result.
+     */
+    @Test
+    public void testGetDetails() throws TmdbException, IOException {
+        String body = TestUtils.readTestFile("api_responses/keywords/details.json");
+        URL url = new URL(TMDB_API_BASE_URL + TMDB_METHOD_KEYWORD + "/1");
+        when(getTmdbUrlReader().readUrl(url, null, RequestType.GET)).thenReturn(body);
+
+        TmdbKeywords tmdbKeywords = getTmdbApi().getKeywords();
+        Keyword keyword = tmdbKeywords.getDetails(1);
+        assertNotNull(keyword);
+        testForNullFieldsAndNewItems(keyword);
+    }
+}
