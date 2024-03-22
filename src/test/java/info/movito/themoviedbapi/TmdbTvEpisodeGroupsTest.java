446861f86ada55962b6e1bcddf273b70c6ce13f7
Conor Egan <68134729+c-eg@users.noreply.github.com>
2024-03-22 23:00:28 +0000

    add test for type enum mapping

--- src/test/java/info/movito/themoviedbapi/TmdbTvEpisodeGroupsTest.java
+++ src/test/java/info/movito/themoviedbapi/TmdbTvEpisodeGroupsTest.java
@@ -1,5 +1,6 @@
 package info.movito.themoviedbapi;
 
+import info.movito.themoviedbapi.model.tv.episodegroups.EpisodeGroupType;
 import info.movito.themoviedbapi.model.tv.episodegroups.TvEpisodeGroups;
 import info.movito.themoviedbapi.tools.RequestType;
 import info.movito.themoviedbapi.tools.TmdbException;
@@ -13,6 +14,7 @@ import java.net.URL;
 import static info.movito.themoviedbapi.TmdbTvEpisodeGroups.TMDB_METHOD_TV_EPISODE_GROUPS;
 import static info.movito.themoviedbapi.tools.ApiUrl.TMDB_API_BASE_URL;
 import static info.movito.themoviedbapi.util.TestUtils.validateAbstractJsonMappingFields;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
@@ -44,5 +46,18 @@ public class TmdbTvEpisodeGroupsTest extends AbstractTmdbApiTest {
         TvEpisodeGroups tvEpisodeGroups = tmdbTvEpisodeGroups.getDetails("5acfef37c3a36842e400333f");
         assertNotNull(tvEpisodeGroups);
         validateAbstractJsonMappingFields(tvEpisodeGroups);
+    }
+
+    /**
+     * Tests the json value of type is mapped to the correct value of {@link EpisodeGroupType}.
+     */
+    @Test
+    public void testEpisodeGroupType() throws IOException, TmdbException {
+        String body = TestUtils.readTestFile("api_responses/tv_episode_groups/details.json");
+        URL url = new URL(TMDB_API_BASE_URL + TMDB_METHOD_TV_EPISODE_GROUPS + "/5acfef37c3a36842e400333f");
+        when(getTmdbUrlReader().readUrl(url, null, RequestType.GET)).thenReturn(body);
+
+        TvEpisodeGroups tvEpisodeGroups = tmdbTvEpisodeGroups.getDetails("5acfef37c3a36842e400333f");
+        assertEquals(EpisodeGroupType.DIGITAL, tvEpisodeGroups.getType());
     }
 }
