9ac99d1b25b5d7127463e1f9b6a763f259ca8390
Conor Egan <68134729+c-eg@users.noreply.github.com>
2024-03-25 01:47:27 +0000

    remove unused imports

--- src/main/java/info/movito/themoviedbapi/model/find/FindTvSeason.java
+++ src/main/java/info/movito/themoviedbapi/model/find/FindTvSeason.java
@@ -1,6 +1,5 @@
 package info.movito.themoviedbapi.model.find;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import info.movito.themoviedbapi.model.tv.core.TvSeason;
 import lombok.Data;
