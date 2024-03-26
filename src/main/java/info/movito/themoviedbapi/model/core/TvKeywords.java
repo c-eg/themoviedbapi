18b1bfa875af717c8b635fcdca2c9b8f8592698a
Conor Egan <68134729+c-eg@users.noreply.github.com>
2024-03-26 22:57:42 +0000

    extend results base class

--- src/main/java/info/movito/themoviedbapi/model/core/TvKeywords.java
+++ src/main/java/info/movito/themoviedbapi/model/core/TvKeywords.java
@@ -1,15 +1,10 @@
 package info.movito.themoviedbapi.model.core;
 
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 import info.movito.themoviedbapi.model.keywords.Keyword;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class TvKeywords extends IdElement {
-    @JsonProperty("results")
-    private List<Keyword> results;
+public class TvKeywords extends Results<Keyword> {
 }
