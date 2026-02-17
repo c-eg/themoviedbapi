043e570a9ecd9cbb9e1f6ee29be6446f66c96428
Conor Egan <17conoregan@gmail.com>
2026-02-17 20:33:08 +0000

    update object mapper config + tests

--- src/main/java/info/movito/themoviedbapi/util/JsonUtil.java
+++ src/main/java/info/movito/themoviedbapi/util/JsonUtil.java
@@ -2,17 +2,22 @@ package info.movito.themoviedbapi.util;
 
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 /**
  * JSON Utility.
  */
 public final class JsonUtil {
-    public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
-        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    public static final ObjectMapper OBJECT_MAPPER = JsonMapper.builder()
+        .defaultSetterInfo(JsonSetter.Value.forValueNulls(Nulls.AS_EMPTY))
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        .build();
 
     private static final ObjectWriter MAP_WRITER = OBJECT_MAPPER.writerFor(Map.class);
 
