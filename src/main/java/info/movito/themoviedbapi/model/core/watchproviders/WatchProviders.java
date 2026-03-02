282f4a01f1b46cf84b84733ea5cfaa4e84022d9a
Fernando A <fernando.foca10@gmail.com>
2026-03-02 21:45:45 +0000

    Apply Optional annotation for all watch provider types

--- src/main/java/info/movito/themoviedbapi/model/core/watchproviders/WatchProviders.java
+++ src/main/java/info/movito/themoviedbapi/model/core/watchproviders/WatchProviders.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import info.movito.themoviedbapi.model.core.AbstractJsonMapping;
+import info.movito.themoviedbapi.model.core.annotation.Optional;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -14,18 +15,23 @@ public class WatchProviders extends AbstractJsonMapping {
     @JsonProperty("link")
     private String link;
 
+    @Optional
     @JsonProperty("rent")
     private List<Provider> rentProviders;
 
+    @Optional
     @JsonProperty("buy")
     private List<Provider> buyProviders = new ArrayList<>();
 
+    @Optional
     @JsonProperty("flatrate")
     private List<Provider> flatrateProviders = new ArrayList<>();
 
+    @Optional
     @JsonProperty("free")
     private List<Provider> freeProviders = new ArrayList<>();
 
+    @Optional
     @JsonProperty("ads")
     private List<Provider> adsProviders = new ArrayList<>();
 }
