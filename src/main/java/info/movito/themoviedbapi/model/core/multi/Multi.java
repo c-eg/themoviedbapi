bd3fb72c5f1aff1e142127b2bc62c7b0529e3c8d
Conor Egan <17conoregan@gmail.com>
2026-05-30 01:16:13 +0100

    add collection to multi

--- src/main/java/info/movito/themoviedbapi/model/core/multi/Multi.java
+++ src/main/java/info/movito/themoviedbapi/model/core/multi/Multi.java
@@ -4,15 +4,13 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 /**
- * Interface that is needed for /search/multi request
- * <p>
- * {@link MultiMovie}, {@link MultiPerson} and {@link MultiTvSeries} implement this interface.</p>
- * <p>Each of them returns corresponding {@link MediaType}</p>
+ * Interface that is needed for /search/multi request.
  *
  * @see info.movito.themoviedbapi.TmdbSearch#searchMulti(String, Boolean, String, Integer)
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "media_type")
 @JsonSubTypes({
+    @JsonSubTypes.Type(value = MultiCollection.class, name = "collection"),
     @JsonSubTypes.Type(value = MultiMovie.class, name = "movie"),
     @JsonSubTypes.Type(value = MultiPerson.class, name = "person"),
     @JsonSubTypes.Type(value = MultiTvSeries.class, name = "tv")
@@ -27,6 +25,7 @@ public interface Multi {
      * The type of media.
      */
     enum MediaType {
+        COLLECTION,
         MOVIE,
         PERSON,
         TV_SERIES
